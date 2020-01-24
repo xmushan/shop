348b5838666e558b7cf3ad8a0b70de5baac702ed
Xmushan <1136053514@qq.com>
2020-01-24 13:33:21 +0800

    third

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <div class="loginBox">
-      <el-form :model="users">
-        <el-form-item label="用户名">
+      <el-form :model="users" :rules="loginRules" ref="loginForm">
+        <el-form-item label="用户名" prop="username">
           <el-input v-model="users.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input v-model="users.password"></el-input>
         </el-form-item>
       </el-form>
@@ -22,22 +22,35 @@ export default {
       users: {
         username: "admin",
         password: "123456"
+      },
+      loginRules: {
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, max: 9, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ]
       }
     };
   },
   methods: {
     getLogin() {
+      this.$refs.loginForm.validate( res => {
+        if (!res) return 
+      })
       this.$http.post("login", this.users).then(res => {
         console.log(res);
         window.sessionStorage.setItem("token", res.data.data.token);
-        this.$router.push('/home');
+        this.$router.push("/home");
       });
     },
-    resert(){
-        this.users.username='';
-        this.users.password="";
+    resert() {
+
+      this.$refs.loginForm.resetFields()
+
     }
-    
   }
 };
 </script>
@@ -51,18 +64,17 @@ export default {
 .loginBox {
   width: 450px;
   height: 450px;
-    background-color: aliceblue;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%);
+  background-color: aliceblue;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
-.el-form{
-    margin-top: 110px;
+.el-form {
+  margin-top: 110px;
 }
-.el-button{
-    float: right;
-    margin-right: 10px;
-   
+.el-button {
+  float: right;
+  margin-right: 10px;
 }
 </style>
